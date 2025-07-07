cf14c4e3c426369d5a28b09285469c5f7686e552
himaz00 <ayhamzbeatz@gmail.com>
2025-07-07 15:43:46 +0200

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Italyna/", // Set this to your repo name
   server: {
     host: "::",
     port: 8080,
